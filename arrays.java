99271270ea940189d7954a905ced26a870802b69
Ishan Gogna <ig2230@nyu.edu>
2022-01-11 00:18:06 -0500

    arrays in java

---
+++ arrays.java
@@ -0,0 +1,10 @@
+class Arrays{
+  public static void main(String args[]){
+    int[] numbers = new int[] {1,2,3,4,5};
+
+    int i = 0;
+    for(i=0;i<numbers.length; i++){
+      System.out.println(numbers[i]);
+    }
+  }
+}
